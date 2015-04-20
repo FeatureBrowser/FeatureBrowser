f97499854b7ad3e0969c60fe01081030305b8909
jmauerhan <jessica@homeceuconnection.com>
2015-04-19 21:08:28 -0500

    Create Generate Command which will generate html files for interactive docs

---
+++ src/FeatureBrowser/Cli/GenerateCommand.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace FeatureBrowser\FeatureBrowser\Cli;
+
+use Symfony\Component\Console\Command\Command as BaseCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class GenerateCommand extends BaseCommand
+{
+}
