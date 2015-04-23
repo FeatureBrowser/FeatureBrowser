164c07d1abc62711b16866f36a7e595e51cfdcc7
jmauerhan <jessica@homeceuconnection.com>
2015-04-23 16:18:32 -0500

    Add option for output

--- src/FeatureBrowser/Cli/GenerateCommand.php
+++ src/FeatureBrowser/Cli/GenerateCommand.php
@@ -3,7 +3,9 @@
 namespace FeatureBrowser\FeatureBrowser\Cli;
 
 use Symfony\Component\Console\Command\Command as BaseCommand;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 final class GenerateCommand extends BaseCommand
@@ -11,10 +13,17 @@ final class GenerateCommand extends BaseCommand
     protected function configure()
     {
         $this->setName('generate')
-             ->setDescription('Generates Feature Browser Documentation');
+             ->setDescription('Generates Feature Browser Documentation')
+             ->addOption('output-dir', 'o', InputOption::VALUE_REQUIRED, 'Where do you want the generated documentation to be stored?');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $outputDir = $input->getOption('output-dir');
+        if(!is_dir($outputDir))
+        {
+            mkdir($outputDir);
+        }
+
     }
 }
