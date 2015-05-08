6298216bc573ebdec6ab1d0fa3cf11cbeda344d6
jmauerhan <jessica@homeceuconnection.com>
2015-05-08 06:35:57 -0500

    Parse feature files, convert into FeatureNodes, and send to template

--- src/FeatureBrowser/Cli/GenerateCommand.php
+++ src/FeatureBrowser/Cli/GenerateCommand.php
@@ -2,17 +2,27 @@
 
 namespace FeatureBrowser\FeatureBrowser\Cli;
 
+use Behat\Gherkin\Keywords\ArrayKeywords;
+use Behat\Gherkin\Lexer;
+use Behat\Gherkin\Parser;
 use Symfony\Component\Console\Command\Command as BaseCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\Yaml\Yaml;
+use Behat\Gherkin\Node\FeatureNode;
 use Twig_Loader_Filesystem;
 use Twig_Environment;
 
 
 final class GenerateCommand extends BaseCommand
 {
+    protected $configFile = 'featurebrowser.yml.dist';
+    protected $projectName;
+    protected $outputDirectory;
+    protected $featuresDirectory;
+
     protected function configure()
     {
         $this->setName('generate')
@@ -20,17 +30,17 @@ final class GenerateCommand extends BaseCommand
              ->addOption('output-dir', 'o', InputOption::VALUE_REQUIRED, 'Where do you want the generated documentation to be stored?');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function loadConfig(InputInterface $input)
     {
         //Read the config file to get default parameters
-        $file        = 'featurebrowser.yml.dist';
-        $configs     = Yaml::parse($file);
-        $projectName = $configs['featurebrowser']['project-name'];
+        $configs                 = Yaml::parse($this->configFile);
+        $this->projectName       = $configs['featurebrowser']['project-name'];
+        $this->featuresDirectory = $configs['featurebrowser']['features-directory'];
 
         $outputDir = $input->getOption('output-dir');
         if(null === $outputDir)
         {
-            $outputDir = $configs['featurebrowser']['output-dir'];
+            $outputDir = $configs['featurebrowser']['output-directory'];
         }
 
         if(!is_dir($outputDir))
@@ -38,14 +48,54 @@ final class GenerateCommand extends BaseCommand
             mkdir($outputDir);
         }
         $outputDir .= (substr($outputDir, -1) == '/' ? '' : '/');
+        $this->outputDirectory = $outputDir;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->loadConfig($input);
+
+        $keywords = new ArrayKeywords(
+            [
+                'en' => [
+                    'name'             => 'English',
+                    'native'           => 'English',
+                    'feature'          => 'Feature|Business Need|Ability',
+                    'background'       => 'Background',
+                    'scenario'         => 'Scenario',
+                    'scenario_outline' => 'Scenario Outline|Scenario Template',
+                    'examples'         => 'Examples|Scenarios',
+                    'given'            => 'Given',
+                    'when'             => 'When',
+                    'then'             => 'Then',
+                    'and'              => 'And',
+                    'but'              => 'But',
+                ]
+            ]
+        );
+
+        $lexer  = new Lexer($keywords);
+        $parser = new Parser($lexer);
+
+        $featuresArray = [];
+
+        $finder = new Finder();
+        $finder->files()->in($this->featuresDirectory)->name('*.feature');
+        foreach($finder as $featureFile)
+        {
+            $featuresArray[] = $parser->parse($featureFile->getContents(), $featureFile->getRealPath());
+        }
 
         $viewsDirectory = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'views';
         $loader         = new Twig_Loader_Filesystem($viewsDirectory, ['cache' => '/cache',]);
         $twig           = new Twig_Environment($loader);
 
-        $templateVariables = ['projectName' => $projectName];
+        $templateVariables = [
+            'projectName' => $this->projectName,
+            'features'    => $featuresArray
+        ];
         $rendered          = $twig->render('base.html.twig', $templateVariables);
-        $filePointer       = fopen($outputDir . 'index.html', 'w');
+        $filePointer       = fopen($this->outputDirectory . 'index.html', 'w');
         fwrite($filePointer, $rendered);
     }
 }
